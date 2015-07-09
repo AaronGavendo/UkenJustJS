e0c4c904cb621651fea0a6d7f9e014de66506f57
AaronGavendo <agavendo@hotmail.com>
2015-07-08 21:30:01 -0400

    4: Change the first element with a ship of your choosing.

--- js/ships.js
+++ js/ships.js
@@ -5,3 +5,19 @@ function check(ship) {
     document.getElementById("selectedship").value = ship;
     console.log(selectedship);
 }
+
+function changeImage() {
+
+	//for(i=1; i<10; i++)
+	//{
+		//console.log(i);
+    	if (document.getElementById("placeShip1").src == "ships/holder.png") 
+    	{
+        	document.getElementById("placeShip1").src = "ships/blueship.png";
+    	}
+    	else 
+    	{
+        	document.getElementById("placeShip1").src = "ships/" +selectedship + "ship.png";
+    	}
+	//}
+}
