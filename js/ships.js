a2288dea3976aeb6c72565c8bd10e5cc33a4a715
AaronGavendo <agavendo@hotmail.com>
2015-07-08 22:44:42 -0400

    5: Get all 9 squares working

--- js/ships.js
+++ js/ships.js
@@ -6,18 +6,38 @@ function check(ship) {
     console.log(selectedship);
 }
 
-function changeImage() {
+function changeImage(pickedElement) {
+	console.log(pickedElement);
 
-	//for(i=1; i<10; i++)
-	//{
-		//console.log(i);
-    	if (document.getElementById("placeShip1").src == "ships/holder.png") 
-    	{
-        	document.getElementById("placeShip1").src = "ships/blueship.png";
-    	}
-    	else 
-    	{
-        	document.getElementById("placeShip1").src = "ships/" +selectedship + "ship.png";
-    	}
-	//}
+	if(pickedElement === 1){
+		    document.getElementById("placeShip1").src = "ships/" +selectedship + "ship.png";
+	}
+	else if(pickedElement === 2){
+		    document.getElementById("placeShip2").src = "ships/" +selectedship + "ship.png";
+	}
+		else if(pickedElement === 3){
+		    document.getElementById("placeShip3").src = "ships/" +selectedship + "ship.png";
+	}
+	else if(pickedElement === 4){
+		    document.getElementById("placeShip4").src = "ships/" +selectedship + "ship.png";
+	}
+	else if(pickedElement === 5){
+		    document.getElementById("placeShip5").src = "ships/" +selectedship + "ship.png";
+	}
+	else if(pickedElement === 6){
+		    document.getElementById("placeShip6").src = "ships/" +selectedship + "ship.png";
+	}
+	else if(pickedElement === 7){
+		    document.getElementById("placeShip7").src = "ships/" +selectedship + "ship.png";
+	}
+	else if(pickedElement === 8){
+		    document.getElementById("placeShip8").src = "ships/" +selectedship + "ship.png";
+	}
+	else if(pickedElement === 9){
+		    document.getElementById("placeShip9").src = "ships/" +selectedship + "ship.png";
+	}
+
+
+
+
 }
