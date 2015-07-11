a81bf782d7f8af7e68a24e6f4c2a1f2cac4f64b0
AaronGavendo <agavendo@hotmail.com>
2015-07-11 14:58:56 -0400

    7: Allow users to change there picks even if they have already selected 5

--- js/ships.js
+++ js/ships.js
@@ -11,7 +11,36 @@ function changeImage(pickedElement) {
 	console.log(clickCount);
 	if(clickCount > 4)
 	{
-		console.log("Inside if > 5");
+		console.log("Inside Greater than 4");
+
+		if((pickedElement === 1) && (selected[0] === 1)){
+		    document.getElementById("placeShip1").src = "ships/" +selectedship + "ship.png";
+		}
+		else if((pickedElement === 2) && (selected[1] === 1)){
+		    document.getElementById("placeShip2").src = "ships/" +selectedship + "ship.png";
+		}
+		else if((pickedElement === 3) && (selected[2] === 1)){
+		    document.getElementById("placeShip3").src = "ships/" +selectedship + "ship.png";
+		}
+		else if((pickedElement === 4) && (selected[3] === 1)){
+		    document.getElementById("placeShip4").src = "ships/" +selectedship + "ship.png";
+		}
+		else if((pickedElement === 5) && (selected[4] === 1)){
+		    document.getElementById("placeShip5").src = "ships/" +selectedship + "ship.png";
+		}
+		else if((pickedElement === 6) && (selected[5] === 1)){
+		    document.getElementById("placeShip6").src = "ships/" +selectedship + "ship.png";
+		}
+		else if((pickedElement === 7) && (selected[6] === 1)){
+		    document.getElementById("placeShip7").src = "ships/" +selectedship + "ship.png";		    
+		}
+		else if((pickedElement === 8) && (selected[7] === 1)){
+		    document.getElementById("placeShip8").src = "ships/" +selectedship + "ship.png";
+		    selected[7] = 1;		    
+		}
+		else if((pickedElement === 9) && (selected[8] === 1)){
+		    document.getElementById("placeShip9").src = "ships/" +selectedship + "ship.png";	    
+		}
 
 	}
 	else{
